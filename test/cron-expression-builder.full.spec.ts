fe9767e3d867b0228a4c5101cd3670c09c78b52a
satyajit <satyajitn15@gmail.com>
2023-11-06 05:43:00 +0530

    Chore/improvements fixes (#9)
    * rename `cronExpBuilder` to `schedule` in all tests.
    
    * refactor `CronExpressionBuilder` class & remove unwanted checks.

--- test/cron-expression-builder.full.spec.ts
+++ test/cron-expression-builder.full.spec.ts
@@ -2,75 +2,73 @@ import { CronExpressionBuilder } from '../src';
 import { CronTimeUnit } from '../src/interfaces';
 
 describe('CronExpressionBuilder - FULL', () => {
-  let cronExpBuilder: CronExpressionBuilder;
+  let schedule: CronExpressionBuilder;
 
   beforeEach(() => {
-    cronExpBuilder = new CronExpressionBuilder();
+    schedule = new CronExpressionBuilder();
   });
 
   test('Run a job at 5:30 PM every day', () => {
-    expect(cronExpBuilder.atTime('17:30').compile()).toStrictEqual(
-      `30 17 * * *`,
-    );
+    expect(schedule.atTime('17:30').compile()).toStrictEqual(`30 17 * * *`);
   });
 
   test('Run at 9 AM on weekdays', () => {
     expect(
-      cronExpBuilder.atTime('09:00').onWeekDays([1, 2, 3, 4, 5]).compile(),
+      schedule.atTime('09:00').onWeekDays([1, 2, 3, 4, 5]).compile(),
     ).toStrictEqual(`0 9 * * 1-5`);
   });
 
   test('Run at noon on the 1st and 15th of the month', () => {
     expect(
-      cronExpBuilder.atTime('12:00').onDaysOfMonth([1, 15]).compile(),
+      schedule.atTime('12:00').onDaysOfMonth([1, 15]).compile(),
     ).toStrictEqual(`0 12 1,15 * *`);
   });
 
   test('Run at midnight during January and July', () => {
     expect(
-      cronExpBuilder.atTime('00:00').duringMonths([1, 7]).compile(),
+      schedule.atTime('00:00').duringMonths([1, 7]).compile(),
     ).toStrictEqual(`0 0 * 1,7 *`);
   });
 
   test('Run every 15 minutes', () => {
-    expect(
-      cronExpBuilder.everyX(15, CronTimeUnit.Minute).compile(),
-    ).toStrictEqual(`*/15 * * * *`);
+    expect(schedule.everyX(15, CronTimeUnit.Minute).compile()).toStrictEqual(
+      `*/15 * * * *`,
+    );
   });
 
   test('Run every day at noon', () => {
-    expect(cronExpBuilder.every('day').atHours([12]).compile()).toStrictEqual(
+    expect(schedule.every('day').atHours([12]).compile()).toStrictEqual(
       `0 12 * * *`,
     );
   });
 
   test('Run every Sunday at 5 PM', () => {
-    expect(
-      cronExpBuilder.onWeekDays([0]).atHours([17]).compile(),
-    ).toStrictEqual(`0 17 * * 0`);
+    expect(schedule.onWeekDays([0]).atHours([17]).compile()).toStrictEqual(
+      `0 17 * * 0`,
+    );
   });
 
   test('Run the 1st day of every month at 1 AM', () => {
     expect(
-      cronExpBuilder.onDaysOfMonth([1]).every('month').atHours([1]).compile(),
+      schedule.onDaysOfMonth([1]).every('month').atHours([1]).compile(),
     ).toStrictEqual(`0 1 1 * *`);
   });
 
   test('Run every weekday at 8:30 AM', () => {
     expect(
-      cronExpBuilder.atTime('08:30').onWeekDays([1, 2, 3, 4, 5]).compile(),
+      schedule.atTime('08:30').onWeekDays([1, 2, 3, 4, 5]).compile(),
     ).toStrictEqual(`30 8 * * 1-5`);
   });
 
   test('Run every 6 hours', () => {
-    expect(cronExpBuilder.everyX(6, CronTimeUnit.Hour).compile()).toStrictEqual(
+    expect(schedule.everyX(6, CronTimeUnit.Hour).compile()).toStrictEqual(
       `0 */6 * * *`,
     );
   });
 
   test('Run every quarter at midnight', () => {
     expect(
-      cronExpBuilder
+      schedule
         .duringMonths([1, 4, 7, 10])
         .onDaysOfMonth([1])
         .atTime('00:00')
@@ -79,36 +77,32 @@ describe('CronExpressionBuilder - FULL', () => {
   });
 
   test('Run every Saturday and Sunday at 10:15 AM', () => {
-    expect(
-      cronExpBuilder.atTime('10:15').onWeekDays([6, 0]).compile(),
-    ).toStrictEqual(`15 10 * * 0,6`);
+    expect(schedule.atTime('10:15').onWeekDays([6, 0]).compile()).toStrictEqual(
+      `15 10 * * 0,6`,
+    );
   });
 
   test('Run at 9 AM, 12 PM, and 3 PM every day', () => {
-    expect(
-      cronExpBuilder.every('day').atHours([9, 12, 15]).compile(),
-    ).toStrictEqual(`0 9,12,15 * * *`);
+    expect(schedule.every('day').atHours([9, 12, 15]).compile()).toStrictEqual(
+      `0 9,12,15 * * *`,
+    );
   });
 
   test('Run at 7 AM, 2 PM, and 10 PM on Tuesdays', () => {
     expect(
-      cronExpBuilder.atHours([7, 14, 22]).onWeekDays([2]).compile(),
+      schedule.atHours([7, 14, 22]).onWeekDays([2]).compile(),
     ).toStrictEqual(`0 7,14,22 * * 2`);
   });
 
   test('Run at 20 past every hour on the 5th of July', () => {
     expect(
-      cronExpBuilder
-        .atMinutes([20])
-        .onDaysOfMonth([5])
-        .duringMonths([7])
-        .compile(),
+      schedule.atMinutes([20]).onDaysOfMonth([5]).duringMonths([7]).compile(),
     ).toStrictEqual(`20 * 5 7 *`);
   });
 
   test('Run every 5 minutes during office hours - using every()', () => {
     expect(
-      cronExpBuilder
+      schedule
         .every('minute')
         .atMinutes([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55])
         .atHours([9, 10, 11, 12, 13, 14, 15, 16])
@@ -118,7 +112,7 @@ describe('CronExpressionBuilder - FULL', () => {
 
   test('Run every 5 minutes during office hours - using everyX()', () => {
     expect(
-      cronExpBuilder
+      schedule
         .everyX(5, CronTimeUnit.Minute)
         .atHours([9, 10, 11, 12, 13, 14, 15, 16])
         .compile(),
@@ -126,8 +120,8 @@ describe('CronExpressionBuilder - FULL', () => {
   });
 
   test('Run at quarter past and quarter to every hour', () => {
-    expect(
-      cronExpBuilder.every('hour').atMinutes([15, 45]).compile(),
-    ).toStrictEqual(`15,45 * * * *`);
+    expect(schedule.every('hour').atMinutes([15, 45]).compile()).toStrictEqual(
+      `15,45 * * * *`,
+    );
   });
 });
